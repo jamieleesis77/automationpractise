18197f92bf97d8840eb168e3586365e001c9eea0
jamieleesis77 <jamieleesis@yahoo.co.uk>
2022-10-11 15:11:39 +0100

    Add API test

---
+++ cypress/e2e/4-API/AutomationPractise.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('API Testing', () => {
+//  beforeEach(() => {
+//    cy.visit(targetURL)
+//  });
+
+  it('gets a 200 status', () => {
+    let APIrequest = cy.request('https://official-joke-api.appspot.com/random_joke');
+    
+    APIrequest
+    .its('status').should('eq',200)
+
+    cy.log(Response)
+  });
+  
+
+  it('gets a response', () => {
+    cy.request('https://official-joke-api.appspot.com/random_joke').then((response => {
+      cy.log(response);
+      console.log(response);
+      expect(response.status).to.equal(200);
+      expect(response.body.data).to.have.length(5);
+    }))
+
+  });
+});
